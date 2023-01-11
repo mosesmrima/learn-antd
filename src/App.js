ca94f093155a57a5337c9e88b59075f0785b7aac
mrima <mrimamss@gmail.com>
2023-01-11 12:14:28 +0300

    functional antd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Users from "./Users"
+
+
+
+export default function App() {
+    return (
+        <div className={"app"}>
+           <Users/>
+        </div>
+    );
+}
