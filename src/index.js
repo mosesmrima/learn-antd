0925ebce566918911e2338f3da2a122048baff3e
mrima <mrimamss@gmail.com>
2023-01-13 16:14:15 +0300

    Wrap app in Browserrouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router} from "react-router-dom";
 
 
 import './index.css';
@@ -7,7 +8,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Router>
+        <App />
+    </Router>
 );
 
 
