5c439cf40f33b5ae50ba1f9e488a9baceab578a9
mrima <mrimamss@gmail.com>
2023-01-16 17:51:02 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,74 +1,31 @@
-import React from "react"
-import {dataArr} from "./data";
-import {Table, Tag, Button} from "antd"
-import {useState, useEffect} from "react";
-import { Routes, Route} from "react-router-dom";
-import {useNavigate} from "react-router"
+import {Routes, Route, Link} from "react-router-dom";
 import FormComp from "./Form"
-import Modal from "./Popup"
 import "antd/dist/antd.css"
 import "./App.css"
+import Home from "./Home"
 import RouteMe from "./RouteMe";
-
+import AntTable from "./Table";
+import EditUser from "./EditUser"
 
  function App () {
-    let [userData, setUserData] = useState([])
-     useEffect(()=>{
-         setUserData(dataArr)
-     }, [])
-
-     const nav = useNavigate()
-     const handleClick = () => {
-        console.log("hey")
-        nav("routeme")
-     }
-
-
-     const columns = [
-            {
-                title: '',
-                dataIndex: 'portrait_data',
-                render:  (text, record) => <Modal src={record.portrait_data}/>,
-                width: "4%"
-            },
-            {
-                title: "Patient No",
-                dataIndex: "patient_number",
-                key: "patient_number"
-            },
-            {
-                title: "Patient Name",
-                dataIndex: "alias_name",
-                key: "alias_name"
-            },
-            {
-                title: "Primary Contact",
-                dataIndex: "primary_contact",
-                key: "primary_contact"
-            },
-            {
-                title: "Residence",
-                dataIndex: "residence",
-                key: "residence"
-            },
-            {
-                title: "Status",
-                dataIndex: "status",
-                key: "status",
-                render: (text, record) =>  <Tag style={{verticalAlign: "middle"}} color={`${record.status==="Active"?"green":"red"}`}>{record.status}</Tag>
-            }
-        ]
-        return (
-                <div>
+     return (
+                <>
+                    <nav>
+                        <ul>
+                            <li> <Link to={"/"}>Home</Link> </li>
+                            <li> <Link to={"/routeme"} > Routeme </Link> </li>
+                            <li> <Link to={"/form"}>Form</Link> </li>
+                            <li> <Link to={"/table"}>Table</Link> </li>
+                        </ul>
+                    </nav>
                     <Routes>
-                        <Route>
-                            <Route path={"routeme"} element={<RouteMe/>}/> 
-                        </Route>
+                        <Route path={"/"} element={<Home/>}/>
+                        <Route path={"/routeme"} element={<RouteMe/>}/>
+                        <Route path={"/form"} element={ <FormComp/>} />
+                        <Route path={"/table"} element={ <AntTable/> } />
+                        <Route path={"/sample"} element={ <EditUser/> }/>
                     </Routes>
-                    {/*<Table dataSource={userData} columns={columns} rowKey={record => record.patient_number} size={"small"}></Table>*/}
-                    <FormComp/>
-
-                </div>
+                    </>
         )
     }
     export default App
