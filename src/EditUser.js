027fe803cc309aaaeb0095e34a3d4038ac011f5d
mrima <mrimamss@gmail.com>
2023-01-17 09:05:46 +0300

    wrap form in card

--- src/EditUser.js
+++ src/EditUser.js
@@ -1,5 +1,5 @@
 import {useLocation, useNavigate} from 'react-router-dom';
-import {Button, Form, Input} from "antd";
+import {Button, Form, Input, Card} from "antd";
 
 const {Item} = Form
 export default function User() {
@@ -13,25 +13,27 @@ export default function User() {
         navigate(-1);
     }
     return (
-      <Form>
-          <Item {...layout}>
-              <Button onClick={handleClick}>Back</Button>
-          </Item>
-          <Item name={"patient_number"} label={"Patient Number"} {...layout}>
-              <Input defaultValue={location.state.data.patient_number}/>
-          </Item>
-          <Item name={"patient_name"} label={"Patient Name"}  {...layout}>
-              <Input defaultValue={location.state.data.alias_name}/>
-          </Item>
-          <Item name={"primary_contact"} label={"Primary Contact"}  {...layout}>
-              <Input defaultValue={location.state.data.primary_contact}/>
-          </Item>
-          <Item name={"residence"}  label={"Residence"}  {...layout}>
-              <Input defaultValue={location.state.data.residence}/>
-          </Item>
-          <Item name={"status"} label={"Status"}  {...layout}>
-              <Input defaultValue={location.state.data.status}/>
-          </Item>
-      </Form>
+        <Card>
+            <Form>
+                <Item {...layout}>
+                    <Button onClick={handleClick}>Back</Button>
+                </Item>
+                <Item name={"patient_number"} label={"Patient Number"} {...layout}>
+                    <Input defaultValue={location.state.data.patient_number}/>
+                </Item>
+                <Item name={"patient_name"} label={"Patient Name"}  {...layout}>
+                    <Input defaultValue={location.state.data.alias_name}/>
+                </Item>
+                <Item name={"primary_contact"} label={"Primary Contact"}  {...layout}>
+                    <Input defaultValue={location.state.data.primary_contact}/>
+                </Item>
+                <Item name={"residence"}  label={"Residence"}  {...layout}>
+                    <Input defaultValue={location.state.data.residence}/>
+                </Item>
+                <Item name={"status"} label={"Status"}  {...layout}>
+                    <Input defaultValue={location.state.data.status}/>
+                </Item>
+            </Form>
+        </Card>
     );
 }
