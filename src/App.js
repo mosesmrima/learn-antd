1886f72c6e12c0592bb0b1d1c6e4431acac43346
mrima <mrimamss@gmail.com>
2023-01-17 09:00:52 +0300

    ad nested routes

--- src/App.js
+++ src/App.js
@@ -15,15 +15,17 @@ import EditUser from "./EditUser"
                             <li> <Link to={"/"}>Home</Link> </li>
                             <li> <Link to={"/routeme"} > Routeme </Link> </li>
                             <li> <Link to={"/form"}>Form</Link> </li>
-                            <li> <Link to={"/table"}>Table</Link> </li>
+                            <li> <Link to={"/patients"}>Patients</Link> </li>
                         </ul>
                     </nav>
                     <Routes>
                         <Route path={"/"} element={<Home/>}/>
                         <Route path={"/routeme"} element={<RouteMe/>}/>
                         <Route path={"/form"} element={ <FormComp/>} />
-                        <Route path={"/table"} element={ <AntTable/> } />
-                        <Route path={"/sample"} element={ <EditUser/> }/>
+                        <Route path={"/patients"}>
+                            <Route index element={ <AntTable/> }/>
+                            <Route path={":pnumber"} element={ <EditUser/> }/>
+                        </Route>
                     </Routes>
                     </>
         )
