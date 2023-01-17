d121152520644c0e85471c6578984007ccccce28
mrima <mrimamss@gmail.com>
2023-01-17 14:25:12 +0300

    fiddle with charts

--- src/Charts.js
+++ src/Charts.js
@@ -1,5 +1,36 @@
+import { Line } from '@ant-design/plots';
+import {useState, useEffect} from "react";
+import {gdp} from "./data2";
+import "./App.css"
 export default function Charts() {
+    const [data, setGdpData] = useState([])
+    useEffect(() => setGdpData(gdp), [])
+
+    const config = {
+        data,
+        xField: 'year',
+        yField: 'gdp',
+        seriesField: 'name',
+        xAxis: {
+            title: {
+                text: "years"
+            }
+        },
+        yAxis: {
+            title: {
+                text: "GDP in dolllars"
+            }
+        },
+    };
+    const china = data.filter(el => {
+       return el.name === "China";
+    })
+    console.log(china)
     return (
-        <h1>Charts</h1>
+        <div className={"plots-line"}>
+            <h1>GDP (antd)</h1>
+            <Line {...config}/>
+            <h1>GDP (mdbb)</h1>
+        </div>
     );
 }
