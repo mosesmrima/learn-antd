33c4b2ddcce3be247f34638b2903a8879d38ad4e
mrima <mrimamss@gmail.com>
2023-01-24 14:58:07 +0300

    navigate to tab1

--- src/Tab3.jsx
+++ src/Tab3.jsx
@@ -1,12 +1,15 @@
 import {Button, Tabs} from "antd";
+import {useNavigate} from "react-router-dom";
 
 const {TabPane} = Tabs
-export const Tab3 = (props) => {
+export const Tab3 = () => {
+    const navigate = useNavigate()
     return (
         <>
             <Tabs defaultActiveKey={"1"} onChange={()=>console.log("change")} onTabClick={()=>console.log("change")}>
                 <TabPane tab={"Tab 1"} key={"1"}>
-                    <Button>Hello</Button>
+
+                    <Button onClick={()=>navigate("/tab1")}>Hello fom 3</Button>
                 </TabPane>
                 <TabPane tab={"Tab 2"} key={"2"}>
                     <Button>Hello</Button>
