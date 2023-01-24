41d8b3800cee865e26a31d641bdb84a488c7b749
mrima <mrimamss@gmail.com>
2023-01-24 14:46:52 +0300

    set default pane using context

--- src/Tab2.jsx
+++ src/Tab2.jsx
@@ -1,14 +1,19 @@
 import {Button, Tabs} from "antd";
+import {RouteContext} from "./App";
+import {useContext} from "react";
 
 const {TabPane} = Tabs
 export const Tab2 = () => {
+    const {activeKey, setActiveKey} = useContext(RouteContext)
     return (
         <>
-            <Tabs defaultActiveKey={"1"}>
+            <Tabs activeKey={activeKey} onTabClick={(key)=> setActiveKey(key)}>
                 <TabPane tab={"Tab 1"} key={"1"}>
+
                     <Button>Hello</Button>
                 </TabPane>
                 <TabPane tab={"Tab 2"} key={"2"}>
+                    This is tab2 <br/>
                     <Button>Hello</Button>
                 </TabPane>
             </Tabs>
