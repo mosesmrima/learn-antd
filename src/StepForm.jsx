97ad7782cf43a322730769c8625f5af1d3b5415e
mrima <mrimamss@gmail.com>
2023-01-25 14:55:29 +0300

    step form but with no persistence

--- src/StepForm.jsx
+++ src/StepForm.jsx
@@ -1,26 +1,91 @@
-import {Steps, Button, message} from "antd";
+import {Steps, Button, message, Form, Input} from "antd";
 import {useState} from "react";
 
 
-export const StepForm = () => {
+const StepForm = (props) => {
     const [current, setCurrent] = useState(0)
+    const {getFieldDecorator} = props.form
+
+
     const steps = [
         {
             title: "Bio Data",
-            content: "step 1"
+            content: <div>
+                <Form.Item label="Name">
+                    {
+                        getFieldDecorator("name",{rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+                <Form.Item label="Email">
+                    {
+                        getFieldDecorator("email", {rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+                <Form.Item label="Phone">
+                    {
+                        getFieldDecorator("phone", {rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+            </div>
         },
         {
             title: "Education",
-            content: "step 2"
+            content: <div>
+                <Form.Item label="School">
+                    {
+                        getFieldDecorator("school", {rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+                <Form.Item label="Degree">
+                    {
+                        getFieldDecorator("degree", {rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+                <Form.Item label="Year">
+                    {
+                        getFieldDecorator("eyear", {rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+            </div>
         },
         {
             title: "Experience",
-            content: "step 3"
+            content: <div>
+                <Form.Item label="Company">
+                    {
+                        getFieldDecorator("company", {rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+                <Form.Item label="Position">
+                    {
+                        getFieldDecorator("position", {rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+                <Form.Item label="Year">
+                    {
+                        getFieldDecorator("year", {rules: [{required: true}]})(<Input/>)
+                    }
+                </Form.Item>
+            </div>
         }
     ]
-    const handlePrevious = () => setCurrent(prevState => prevState - 1)
-    const handleNext = () => setCurrent(prevState => prevState + 1)
-    const handleFinish = () => message.success("Submitted Successfully")
+    const handlePrevious = (e) => {
+        setCurrent(prevState => prevState - 1)
+    }
+    const handleNext = (e) => {
+        e.preventDefault()
+        setCurrent(prevState => prevState + 1)
+    }
+    const handleFinish = (e) => {
+        e.preventDefault()
+        props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values)
+            }
+        })
+    }
+
+
     return (
         <div>
             <Steps current={current}>
@@ -28,19 +93,25 @@ export const StepForm = () => {
                     steps.map((el ,index)=> <Steps.Step key={index} title={el.title}/>)
                 }
             </Steps>
-            {
-                steps[current].content
-            }
-            {
-                current > 0 && <Button type={"primary"} onClick={handlePrevious} style={{margin: 8}}>Previous</Button>
-            }
-            {
-                current < steps.length - 1 && <Button type="primary" onClick={handleNext} style={{margin: 8}}>Next</Button>
-            }
-            {
-                current === steps.length - 1 && <Button type={"primary"} onClick={handleFinish} style={{margin: 8}}>Finish</Button>
-            }
+            <Form onSubmit={handleFinish}>
+                {
+                    steps[current].content
+                }
+                {
+                    current > 0 && <Button type={"primary"} onClick={(e) => handlePrevious(e)} style={{margin: 8}}>Previous</Button>
+                }
+                {
+                    current < steps.length - 1 && <Button type="primary" onClick={(e) => handleNext(e)} style={{margin: 8}}>Next</Button>
+                }
+                {
+                    current === steps.length - 1 && <Button htmlType={"submit"} onClick={(e) => handleFinish(e)} style={{margin: 8}}>Finish</Button>
+                }
+            </Form>
+
 
         </div>
     );
 }
+
+const Resume = Form.create({name: "Resume"})(StepForm)
+export default Resume
